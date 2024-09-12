52316b493072f71c7d4fbf20a6998d1aeec79f15
Nolaj <davidjalon@protonmail.com>
2024-09-12 15:33:21 +0200

    Añadida la función para crear la buylist

--- main.py
+++ main.py
@@ -23,12 +23,7 @@ def leer_mazo(ruta_archivo):
                 # Separar la cantidad del nobmre de la carta
                 cantidad, nombre_carta = linea.strip().split(' ', 1)
                 mazo.append((int(cantidad), nombre_carta))
-
     return mazo
-
-def comparar_mazo(ruta_archivo_mazo):
-    mazo = leer_mazo
-
 
 def comparar_mazo_con_coleccion(ruta_archivo_mazo):
     mazo = leer_mazo(ruta_archivo_mazo)
@@ -49,5 +44,32 @@ def comparar_mazo_con_coleccion(ruta_archivo_mazo):
         else:
             print(f"La carta '{nombre_carta_mazo}' no está en la colección.")
 
-# Llamar a la función con el archivo de texto del mazo
-comparar_mazo_con_coleccion('Grixis Affinity.txt')
+def crear_buylist(ruta_archivo_mazo, ruta_archivo_salida):
+    mazo = leer_mazo(ruta_archivo_mazo)
+
+    cartas_faltantes = []
+
+    for cantidad_mazo, nombre_carta_mazo in mazo:
+            # Limpiar y estandarizar el nombre de la carta
+            nombre_carta_mazo_limpio = nombre_carta_mazo.strip().lower()
+            
+            # Buscar todas las coincidencias en el DataFrame
+            cartas_en_coleccion = df[df['Name'].str.strip().str.lower().str.contains(nombre_carta_mazo_limpio, na=False)]
+
+            if not cartas_en_coleccion.empty:
+                cantidad_total = cartas_en_coleccion['Quantity'].sum()
+                if cantidad_total < cantidad_mazo:
+                    faltante = cantidad_mazo - cantidad_total
+                    cartas_faltantes.append(f"Faltan {faltante} unidades de '{nombre_carta_mazo}'")
+            else:
+                cartas_faltantes.append(f"La carta'{nombre_carta_mazo}' no está en la colección")
+
+    # Escribir la lista de cartas en un archivo.txt
+    with open(ruta_archivo_salida, 'w') as archivo_salida:
+        for carta_faltante in cartas_faltantes:
+            archivo_salida.write(carta_faltante + '\n')
+                
+
+
+# Llamar a la función con las rutas de los archivos
+crear_buylist('Grixis Affinity.txt', 'cartas_faltantes.txt')
